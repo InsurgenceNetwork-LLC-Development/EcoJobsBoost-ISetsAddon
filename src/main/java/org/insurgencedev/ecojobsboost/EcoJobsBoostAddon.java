48550870bc754d1cd093cbb8d53daae631ae870b
iHxtch <84823076+iHxtch@users.noreply.github.com>
2023-11-25 20:08:20 -0500

    init

---
+++ src/main/java/org/insurgencedev/ecojobsboost/EcoJobsBoostAddon.java
@@ -0,0 +1,21 @@
+package org.insurgencedev.ecojobsboost;
+
+import org.bukkit.Bukkit;
+import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
+import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
+import org.insurgencedev.insurgencesets.libs.fo.Common;
+
+@ISetsAddon(name = "EcoJobsBoost", version = "1.0.0", author = "Insurgence Dev Team", description = "Boost the job experience earned from EcoJobs")
+public class EcoJobsBoostAddon extends InsurgenceSetsAddon {
+
+    @Override
+    public void onAddonStart() {
+        if (isDependentEnabled()) {
+            registerEvent(new JobExperienceGainListener());
+        }
+    }
+
+    private boolean isDependentEnabled() {
+        return Bukkit.getPluginManager().isPluginEnabled("EcoJobs");
+    }
+}
