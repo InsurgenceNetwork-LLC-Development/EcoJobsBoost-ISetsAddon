db549f6ee1fc4ac0c8e3312c6b9a3340cc6fefd6
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-30 22:20:50 -0500

    account for decimal values

--- src/main/java/org/insurgencedev/ecojobsboost/EcoJobsBoostAddon.java
+++ src/main/java/org/insurgencedev/ecojobsboost/EcoJobsBoostAddon.java
@@ -5,7 +5,7 @@ import org.insurgencedev.insurgencesets.api.addon.ISetsAddon;
 import org.insurgencedev.insurgencesets.api.addon.InsurgenceSetsAddon;
 import org.insurgencedev.insurgencesets.libs.fo.Common;
 
-@ISetsAddon(name = "EcoJobsBoost", version = "1.0.1", author = "Insurgence Dev Team", description = "Boost the job experience earned from EcoJobs")
+@ISetsAddon(name = "EcoJobsBoost", version = "1.0.2", author = "Insurgence Dev Team", description = "Boost the job experience earned from EcoJobs")
 public class EcoJobsBoostAddon extends InsurgenceSetsAddon {
 
     @Override
